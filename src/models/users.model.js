d81068343c3131a4ea1d73c4bf6c9df203a7bdce
vinicius cardoso <cardosovinicius87@gmail.com>
2022-09-23 16:22:35 -0300

    feat: migration and model users

---
+++ src/models/users.model.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      display_name: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    });
+  
+    return User;
+  };
