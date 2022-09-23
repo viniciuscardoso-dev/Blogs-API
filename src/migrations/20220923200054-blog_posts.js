d4421d7b7cebdcf906204a52be232d579a99e094
vinicius cardoso <cardosovinicius87@gmail.com>
2022-09-23 17:11:18 -0300

    feat: migration and model blog_posts

---
+++ src/migrations/20220923200054-blog_posts.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('blog_posts');
+  }
+};
---
+++ src/models/blog_posts.model.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: false,
+      tableName: 'blog_posts',
+      underscored: true,
+    });
+  
+    BlogPost.associate = (models) => {
+        BlogPost.belongsTo(models.User, { 
+          constraint: true,
+          foreignKey: 'user_id',
+        });
+    };
+  
+    return Rotina;
+  };
