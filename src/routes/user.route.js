b15c8345921bd69f3b84fed37026537453e67fb4
vinicius cardoso <cardosovinicius87@gmail.com>
2022-09-24 22:31:15 -0300

    fix: rota post /user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userController = require('../controllers/user.controller.js'); 
+const userMiddleware = require('../middlewares/fields/user.field');
 
 const routers = express.Router();
 
-routers.post('/', userController.createUser);
+routers.post('/', userMiddleware.newUser, userController.createUser);
 
 module.exports = routers;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,19 +1,19 @@
 const { User, sequelize } = require('../models');
 
 const createUser = async ({ displayName, email, password, image }) => {
-    const result = await sequelize.transaction(async (t) => {
-      const newUser = await User.create(
-        { displayName, email, password, image },
-        {
-          transaction: t,
-          underscored: true,
-        },
-        );
-        return newUser;
-    });
-    return result;
+  const result = await sequelize.transaction(async (t) => {
+    const newUser = await User.create(
+      { displayName, email, password, image },
+      {
+        transaction: t,
+        underscored: true,
+      },
+    );
+    return newUser;
+  });
+  return result;
 };
 
 module.exports = {
-    createUser,
+  createUser,
 };
